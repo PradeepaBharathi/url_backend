0990eb10447d4818f787e6c505fed8e1e06185ea
PradeepaBharathi <bharathi83201@gmail.com>
2023-09-14 21:41:31 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+console.log("URL Shortener");
+import express from "express"
+import dotenv from "dotenv";
+import cors from "cors";
+
+import { urlRouter } from "./Routes/urls.js";
+import { isAuthenticated } from "./Authentication/user_auth.js";
+import { getURL } from './Controller/urls.js';
+import { userRouter } from "./Routes/users.js";
+
+dotenv.config();
+const PORT = process.env.PORT;
+const app = express(); 
+
+//application middleware 
+app.use(express.json());
+app.use(cors());
+
+//user is the base route 
+app.use("/user", userRouter);
+app.use("/url", isAuthenticated, urlRouter);
+
+app.get("/", (req,res)=> {
+   res.send({msg:"connection working - URL shortener app"});
+})
+
+// To get URL redirection from short URL 
+app.get('/:urlID', async(req, res)=>{
+   try{
+       const url = await getURL({urlID: req.params.urlID})
+       if(url){
+           console.log("redirecting");
+           return res.status(200).json({longURL:url.longURL})
+         //return res.redirect(url.longURL)
+       }
+       else{
+           return res.status(404).json({message: 'No URL Found'})
+       }
+   }
+   catch(err){
+       console.error(err)
+       res.status(500).json('Server Error')
+   }
+ })
+ 
+app.listen(PORT,()=>console.log(`Server started at localhost:${PORT}`))
